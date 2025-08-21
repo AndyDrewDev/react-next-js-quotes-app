b3e6da94294e67bb556589abf9c844a1c0d69c0e
AndyDrewDev <andydrew.stamp@gmail.com>
2025-08-21 10:54:16 +0300

    Added Navbar component to layout and adjusted main content styling; modified Button component margin for better alignment.

--- client/components/Button.jsx
+++ client/components/Button.jsx
@@ -1,6 +1,6 @@
 export default function Button({ onClick, text }) {
   return (
-    <div className='text-center m-6'>
+    <div className='text-center m-5'>
       <button
         onClick={onClick}
         className='bg-violet-900 text-white text-xl px-6 py-3 rounded-lg hover:bg-violet-800 focus-outline-none focus:ring-2 focus:ring-violet-600'
